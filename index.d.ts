fd1c9d23fb8fefd21f45244bc16919bfaadc7d2f
Valentin Hervieu <valentin.hervieu@ricardo.ch>
2021-10-21 23:11:12 +0200

    Use suggestion

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,20 @@
-export type InitOptions = {
-  /**
-   * This function is called for every console warn/error.
-   * If true is returned, the message will not show in the console
-   * and the test won't fail.
-   */
-  silenceMessage?: (message: string, methodName: 'warn' | 'error' | 'log') => boolean
-  /** defaults to true */
-  shouldFailOnWarn?: boolean
-  /** defaults to true */
-  shouldFailOnError?: boolean
-  /** defaults to false */
-  shouldFailOnLog?: boolean
+declare namespace init {
+  type InitOptions = {
+    /**
+     * This function is called for every console warn/error.
+     * If true is returned, the message will not show in the console
+     * and the test won't fail.
+     */
+    silenceMessage?: (message: string, methodName: 'warn' | 'error' | 'log') => boolean
+    /** defaults to true */
+    shouldFailOnWarn?: boolean
+    /** defaults to true */
+    shouldFailOnError?: boolean
+    /** defaults to false */
+    shouldFailOnLog?: boolean
+  }
 }
-declare function init(options?: InitOptions): void
+
+declare function init(options?: init.InitOptions): void
 
 export = init
