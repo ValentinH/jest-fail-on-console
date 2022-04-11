97fb5b0990d4dd218e3d4ab6e9c23e1aa5135048
Florian Wendelborn <1133858+FlorianWendelborn@users.noreply.github.com>
2022-04-11 10:59:16 +0200

    feature(#16): Add Support for console.debug & console.info (#17)

--- index.d.ts
+++ index.d.ts
@@ -1,25 +1,42 @@
 declare namespace init {
   type InitOptions = {
     /**
-     * This function is called for every console warn/error.
-     * If true is returned, the message will not show in the console
-     * and the test won't fail.
-     */
-    silenceMessage?: (message: string, methodName: 'warn' | 'error' | 'log') => boolean
-    /** defaults to true */
-    shouldFailOnWarn?: boolean
-    /** defaults to true */
-    shouldFailOnError?: boolean
-    /** defaults to false */
-    shouldFailOnLog?: boolean
-    /** defaults to false */
-    shouldFailOnAssert?: boolean
-    /**
      * This function lets you define a custom error message. The methodName is the method
      * that caused the error, bold is a function that lets you bold subsets of your message.
      * example: (methodName, bold) => `console.${methodName} is not ${bold('allowed')}`
      */
-    errorMessage?: (methodName: string, bold: (string) => string) => string
+    errorMessage?: (
+      methodName: 'assert' | 'debug' | 'error' | 'info' | 'log' | 'warn',
+      bold: (string) => string
+    ) => string
+
+    /** @default false */
+    shouldFailOnAssert?: boolean
+
+    /** @default false */
+    shouldFailOnDebug?: boolean
+
+    /** @default true */
+    shouldFailOnError?: boolean
+
+    /** @default false */
+    shouldFailOnInfo?: boolean
+
+    /** @default false */
+    shouldFailOnLog?: boolean
+
+    /** @default true */
+    shouldFailOnWarn?: boolean
+
+    /**
+     * This function is called for every console warn/error.
+     * If true is returned, the message will not show in the console
+     * and the test won't fail.
+     */
+    silenceMessage?: (
+      message: string,
+      methodName: 'assert' | 'debug' | 'error' | 'info' | 'log' | 'warn'
+    ) => boolean
   }
 }
 
